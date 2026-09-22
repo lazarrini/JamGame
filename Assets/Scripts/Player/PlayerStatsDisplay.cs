18bf47111ea3be15e81aa30a7c1101f990f8e254
Лазарь <139632502+lazarrini@users.noreply.github.com>
2026-09-22 18:52:58 +0300

    me

---
+++ Assets/Scripts/Player/PlayerStatsDisplay.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerStatsDisplay : MonoBehaviour
+{
+    [SerializeField] private Image foodBar;
+    [SerializeField] private Image hearthBar;
+    [SerializeField] private Image areaBar;
+    
+    
+}
